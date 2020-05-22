00d80d8c1a860e6f3dfe20cf7df30e3eca91600e
Alexander Brock <alexander@euler>
2020-05-22 14:24:07 +0200

    Print width and height

--- src/bin/marker_gen.cpp
+++ src/bin/marker_gen.cpp
@@ -586,6 +586,12 @@ int main(int argc, char* argv[])
   else if (argc >= 8) {
       int w = atoi(argv[3]);
       int h = atoi(argv[4]);
+      if (w <= 0) {
+          std::cout << "width is " << w << " <= 0." << std::endl;
+      }
+      if (h <= 0) {
+          std::cout << "height is " << h << " <= 0." << std::endl;
+      }
       assert(w && h);
       Mat_<uint8_t> img(h*32*5, w*32*5, uint8_t(255));
       int limit_width = 32;
